636f9608d201177cea1317c88305a0dde743f4c2
umisuna <blhx@danmu9.com>
2019-04-28 14:31:29 +0800

    support replace ui image

--- src/modules/resourse.js
+++ src/modules/resourse.js
@@ -1,15 +1,18 @@
 import { log } from '../utils/index'
+import getImage from '../store/image'
+import config from '../config'
 
 export default async function resourceHook () {
+  const imageMap = await getImage()
   const originLoadElement = aoba.loaders.Resource.prototype._loadElement
   aoba.loaders.Resource.prototype._loadElement = function (type) {
     // if (type === 'image' && this.url.includes('8f5a4652a6d1d7a160fa5')) {
     //   log(this.url, this.name)
     // }
-    // if (this.name === 'images/ui/common/parts_buttons.json_image') {
-    //   this.url = 'https://biuuu.github.io/ShinyColors/data/123.png'
-    //   this.crossOrigin = true
-    // }
+    if (imageMap.has(this.name)) {
+      this.url = `${config.origin}/data/image/${imageMap.get(this.name)}?V=${config.hash}`
+      this.crossOrigin = true
+    }
     return originLoadElement.call(this, type)
   }
 }
