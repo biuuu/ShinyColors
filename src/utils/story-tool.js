3e6dabc7c59d8927e0e16726b4fb584b0282ba0a
umisuna <blhx@danmu9.com>
2019-05-23 11:38:19 +0800

    update story btn

--- src/utils/story-tool.js
+++ src/utils/story-tool.js
@@ -9,66 +9,68 @@ const html = `
   #sczh-story-tool {
     position: absolute;
     display: none;
-    background: #f3f5fe;
-    border-radius: 20%;
-    border: 2px solid rgba(78, 144, 104, 0.7);
+    background: #ffffff;
+    border-radius: 24px;
     box-sizing: border-box;
     font-family: sczh-yuanti;
     align-items: center;
     justify-content: center;
-    color: #409591;
-    text-shadow: 0 0 7px #fff;
+    color: #ff6499;
+    text-shadow: 0 0 6px #fff;
     cursor: pointer;
     user-select: none;
+    width: 100px;
+    height: 100px;
+    font-size: 32px;
+    border: 7px solid transparent;
+    border-image: url(${config.origin}/data/image/border.png);
+    border-image-slice: 7;
+    transform-origin: top right;
   }
   .story-tool-btns {
-    position: absolute;
-    width: 240%;
+    width: 100%;
     height: 100%;
     display: none;
-    right: -2px;
-    top: -2px;
   }
   .story-tool-btns .btn-download-sczh,
   .story-tool-btns label {
     flex: 1;
     height: 100%;
     background: #fff;
-    border-radius: 20%;
-    border: 2px solid rgba(78, 144, 104, 0.7);
     display: flex;
     box-sizing: content-box;
     align-items: center;
     justify-content: center;
     cursor: pointer;
-    color: #409591;
-    text-shadow: 0 0 7px #fff;
+    color: #c0aade;
+    text-shadow: 0 0 6px #fff;
   }
-  .story-tool-btns .btn-download-sczh {
-    border-radius: 0 20% 20% 0;
-    border-left: 1px solid rgba(0, 0, 0, 0.1);
+  .story-tool-btns .btn-download-sczh:hover {
+    color: #9f66ec;
   }
   .story-tool-btns label {
-    border-radius: 20% 0 0 20%;
-    color: rgba(250, 43, 101, 0.52);
-    border: 2px solid rgba(250, 43, 101, 0.52);
-    border-right: 1px solid rgba(0, 0, 0, 0.1);
+    color: rgb(242, 156, 199);
+    border-right: 1px solid #c9c9c9;
   }
   #sczh-story-tool .btn-close-sczh {
-    width: 65%;
-    height: 35%;
+    height: 25px;
+    width: 50px;
     background: rgba(0, 0, 0, 0.58);
     color: #fff;
+    letter-spacing: 2px;
     position: absolute;
-    right: -28%;
-    top: -27%;
-    border-radius: 10%;
+    right: -25px;
+    top: -20px;
+    border-radius: 4px;
     display: none;
     align-items: center;
     justify-content: center;
     z-index: 1;
     font-family: sczh-heiti;
-    font-size: 0.4em;
+    font-size: 15px;
+  }
+  #sczh-story-tool:hover {
+    width: 200px;
   }
   #sczh-story-tool:hover .story-tool-btns {
     display: flex;
@@ -78,6 +80,16 @@ const html = `
   }
   #sczh-story-tool:hover > .text-sczh {
     display: none;
+  }
+  #sczh-story-tool .btn-close-sczh:hover {
+    background: rgba(0, 0, 0, 0.9);
+  }
+  .story-tool-btns label:hover {
+    color: #f270b1;
+  }
+  .story-tool-btns .btn-download-sczh:hover,
+  .story-tool-btns label:hover {
+    background-color: #f7f7f7;
   }
   </style>
   <div id="sczh-story-tool"><span class="text-sczh">剧情</span>
@@ -124,9 +136,10 @@ const showStoryTool = (storyCache) => {
     }
     cont.style.right = Math.floor(offsetRight + pos[0] * cw) + 'px'
     cont.style.top = Math.floor(offsetTop + pos[1] * ch) + 'px'
-    cont.style.width = Math.floor(size[0] * cw) + 'px'
-    cont.style.height = Math.floor(size[1] * cw) + 'px'
-    cont.style.fontSize = Math.floor(size[1] * cw * 0.35) + 'px'
+    cont.style.transform = `scale(${(ch/900).toFixed(3)})`
+    // cont.style.width = Math.floor(size[0] * cw) + 'px'
+    // cont.style.height = Math.floor(size[1] * cw) + 'px'
+    // cont.style.fontSize = Math.floor(size[1] * cw * 0.35) + 'px'
     if (storyCache.name) {
       cont.style.display = 'flex'
     } else {
