bc29f772df9ae87e6af44d55176b13205cf59d0f
mio <10892119+biuuu@users.noreply.github.com>
2019-12-16 11:24:25 +0800

    Fix script execution timing issue on low speed networks

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import resourceHook from './modules/resourse'
 import transScenario from './modules/scenario'
 import addFont from './utils/fontFace'
 import './utils/keepBgm'
-import { log } from './utils/index'
+import { log, sleep } from './utils/index'
 
 const main = async () => {
   try {
@@ -15,8 +15,20 @@ const main = async () => {
   }
 }
 
+let waitCount = 0
+const start = async () => {
+  if ((unsafeWindow && unsafeWindow.ezg || window.ezg) && waitCount < 30) {
+    await sleep(1000)
+    waitCount++
+    log(waitCount)
+    await start()
+  } else {
+    main()
+  }
+}
+
 if (window.unsafeWindow) {
-  unsafeWindow.addEventListener('load', main)
+  unsafeWindow.addEventListener('load', start)
 } else {
-  window.addEventListener('load', main)
+  window.addEventListener('load', start)
 }
