c786007dca566ebac87c45acf6a6784b55ffc435
mio <10892119+biuuu@users.noreply.github.com>
2019-12-26 15:04:42 +0800

    update searchModule script

--- script/searchModule.js
+++ script/searchModule.js
@@ -1,18 +1,27 @@
-// player = 64  textPlayer
-// load scenario = 260  load
-// request = 2  request
-// phrase = 4
-let log
+var log
 if (!log) {
   const iframe = document.createElement('iframe')
   iframe.style.display = 'none'
   document.body.appendChild(iframe)
   log = iframe.contentWindow.console.log
 }
-var keyword = 'load'
-for (let i = 0; i < 900; i++) {
-  let module = primJsp([],[],[i])
-  if (module && (module[keyword] || (module.default && module.default[keyword]))) {
-    log(i, module)
+
+const getModule = async (name, condition) => {
+  for (let i = 1; i < 500; i++) {
+    let module = primJsp([], [], [i])
+    if (module && condition(module)) {
+      log(`${name}: ${i}`)
+      break
+    }
   }
 }
+
+getModule('REQUEST', (module) => {
+  return module.default && module.default.get && module.default.post && module.default.put && module.default.patch
+})
+getModule('PHRASE', (module) => {
+  return module.default && module.default._polyglot && module.default._polyglot.phrases
+})
+getModule('SCENARIO', (module) => {
+  return module.default && module.default['load'] && module.default['_errorEvent'] && module.default['_handleError']
+})
