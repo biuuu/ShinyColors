1e49a6dbc73911bd0c6993f8d5c9954576292763
mio <10892119+biuuu@users.noreply.github.com>
2020-04-30 17:07:16 +0800

    Update: empty code temporarily

--- src/main.js
+++ src/main.js
@@ -1,34 +1,34 @@
-import transPhrase from './modules/phrase'
-import watchText from './modules/text'
-import requestHook from './modules/request'
-import resourceHook from './modules/resourse'
-import transScenario from './modules/scenario'
-import addFont from './utils/fontFace'
-import './utils/keepBgm'
-import { log, sleep } from './utils/index'
+// import transPhrase from './modules/phrase'
+// import watchText from './modules/text'
+// import requestHook from './modules/request'
+// import resourceHook from './modules/resourse'
+// import transScenario from './modules/scenario'
+// import addFont from './utils/fontFace'
+// import './utils/keepBgm'
+// import { log, sleep } from './utils/index'
 
-const main = async () => {
-  try {
-    await Promise.all([resourceHook(), addFont(), transPhrase(), watchText(), requestHook(), transScenario()])
-  } catch (e) {
-    log(e)
-  }
-}
+// const main = async () => {
+//   try {
+//     await Promise.all([resourceHook(), addFont(), transPhrase(), watchText(), requestHook(), transScenario()])
+//   } catch (e) {
+//     log(e)
+//   }
+// }
 
-let waitCount = 0
-const start = async () => {
-  if ((window.unsafeWindow && window.unsafeWindow.ezg || window.ezg) && waitCount < 300) {
-    await sleep(100)
-    waitCount++
-    if (waitCount % 10 === 0) log(`Waiting: ${waitCount / 10}s`)
-    await start()
-  } else {
-    main()
-  }
-}
+// let waitCount = 0
+// const start = async () => {
+//   if ((window.unsafeWindow && window.unsafeWindow.ezg || window.ezg) && waitCount < 300) {
+//     await sleep(100)
+//     waitCount++
+//     if (waitCount % 10 === 0) log(`Waiting: ${waitCount / 10}s`)
+//     await start()
+//   } else {
+//     main()
+//   }
+// }
 
-if (window.unsafeWindow) {
-  window.unsafeWindow.addEventListener('load', start)
-} else {
-  window.addEventListener('load', start)
-}
+// if (window.unsafeWindow) {
+//   window.unsafeWindow.addEventListener('load', start)
+// } else {
+//   window.addEventListener('load', start)
+// }
