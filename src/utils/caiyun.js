5b9bc84fc629a43f3d4c5473d5b3a1896cfccdfa
mio <10892119+biuuu@users.noreply.github.com>
2020-07-23 10:18:53 +0800

    perf: remove fingerprintjs2

--- src/utils/caiyun.js
+++ src/utils/caiyun.js
@@ -1,77 +1,62 @@
-import Fingerprint2 from 'fingerprintjs2/dist/fingerprint2.min'
 import request from './request'
 import { fetchInfo } from './fetch'
+import x64hash128 from './x64hash128'
 
-let defaultUid = '5a096eec830f7876a48aac47'
 let bid = ''
 let uid = ''
 let pid = ''
 let auth = null
+let limited = false
 
-const sleep = (time) => {
-  return new Promise(rev => {
-    setTimeout(rev, time)
-  })
+const setBid = () => {
+  let str = '0123456789abcdefghijklmnopqrstuvwxyz'
+  let text = ''
+  for (let i = 0; i < 33; i++) {
+    text += str[Math.floor(Math.random() * str.length)]
+  }
+  bid = x64hash128(text, 31)
+  localStorage.setItem('sczh:bid', bid)
 }
 
-const testCookies = async () => {
-  const res = await request('https://biz.caiyunapp.com/test_cookies', {
+try {
+  bid = localStorage.getItem('sczh:bid')
+} catch (e) {}
+
+if (!bid) {
+  setBid()
+}
+
+const getAuth = async () => {
+  const res = await request('https://api.interpreter.caiyunai.com/v1/page/auth', {
     cors: true,
-    credentials: 'include',
+    method: 'POST',
     headers: {
-      'X-Authorization': `token ${fetchInfo.data.cyweb_token}`
-    }
-  })
-  if (res.status === 'ok' && res.cookies.cy_user) {
-    const data =  JSON.parse(decodeURIComponent(res.cookies.cy_user))
-    uid = data._id || defaultUid
-  } else {
-    return false
-  }
-}
-
-const getAuth = () => {
-  if (!auth) {
-    auth = new Promise((rev, rej) => {
-      testCookies().then(async () => {
-        if (!uid && !bid) {
-          const components = await Fingerprint2.getPromise({
-            excludes: { fonts: true, canvas: true, webgl: true, audio: true}
-          })
-          let values = components.map(function (component) { return component.value })
-          bid = Fingerprint2.x64hash128(values.join(''), 31)
-        }
-        return request('https://api.interpreter.caiyunai.com/v1/page/auth', {
-          cors: true,
-          method: 'POST',
-          headers: {
-            'X-Authorization': `token ${fetchInfo.data.cyweb_token}`,
-            'Content-Type': 'application/json'
-          },
-          data: JSON.stringify({
-            browser_id: bid,
-            device_id: '',
-            os_type: 'web',
-            title: document.title,
-            url: document.URL,
-            user_id: uid
-          })
-        })
-      }).then(res => {
-        if (res.auth_type === -1 || !res.page_id) {
-          throw new Error('Caiyun api out of limit.')
-        } else {
-          pid = res.page_id
-        }
-      }).then(rev).catch(rej)
+      'X-Authorization': `token ${fetchInfo.data.cyweb_token}`,
+      'Content-Type': 'application/json'
+    },
+    data: JSON.stringify({
+      browser_id: bid,
+      device_id: '',
+      os_type: 'web',
+      title: document.title,
+      url: document.URL,
+      user_id: uid
     })
+  })
+  if (res.auth_type === -1 || !res.page_id) {
+    limited = true
+    setBid()
+  } else {
+    pid = res.page_id
   }
-  return auth
 }
 
 const translator = async (list, from = 'ja') => {
   await getAuth()
   await auth
+  if (limited) {
+    return ['caiyunoutoflimit']
+  }
   const res = await request('https://api.interpreter.caiyunai.com/v1/page/translator', {
     cors: true,
     method: 'POST',
@@ -90,7 +75,7 @@ const translator = async (list, from = 'ja') => {
       url: document.URL
     })
   })
-  if (res?.target) {
+  if (res && res.target) {
     return res.target.map(item => item.target)
   }
   return []
