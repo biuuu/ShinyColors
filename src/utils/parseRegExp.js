5bdb5d803b95278f988a6d5b2b3e2bc6248ff480
mio <10892119+biuuu@users.noreply.github.com>
2020-08-10 22:36:03 +0800

    feat: add a regexp type ''

--- src/utils/parseRegExp.js
+++ src/utils/parseRegExp.js
@@ -1,6 +1,7 @@
 const numRE = '([+-＋－]?[0-9０-９\u2460-\u2500]{1,10}\\.?[0-9０-９\u2460-\u2500]{0,4}?)'
 const percentRE = '([+-＋－]?[0-9０-９]{1,10}\\.?[0-9０-９]{0,4}?[%％])'
 const unknownRE = '(.+?)'
+const sepRE = '.?'
 
 const parseRegExp = (str, list = []) => {
   let result = str.replace(/\./g, '\\.')
@@ -8,6 +9,7 @@ const parseRegExp = (str, list = []) => {
     .replace(/\$percent/g, percentRE)
     .replace(/\$unknown/g, unknownRE)
     .replace(/\$uk/g, unknownRE)
+    .replace(/\$sep/g, sepRE)
   list.forEach(item => {
     result = result.replace(item.re, item.exp)
     item.re.lastIndex = 0
