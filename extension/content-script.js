72f23991c600bdb595d6ecc8e31e31ece256975e
mio <10892119+biuuu@users.noreply.github.com>
2022-04-29 11:25:42 +0800

    feat: add a extension

---
+++ extension/content-script.js
@@ -0,0 +1,22 @@
+const mutationCallback = (mutationsList) => {
+  for (let mutation of mutationsList) {
+    const addedNodes = mutation.addedNodes
+    addedNodes.forEach(node => {
+      if ('SCRIPT' === node.tagName) {
+        if (node.src.includes('shinycolors.enza.fun/app')) {
+          node.async = true
+          observer.disconnect()
+        }
+      }
+    })
+  }
+}
+
+const obConfig = {
+  subtree: true,
+  childList: true
+}
+
+const targetNode = document
+const observer = new MutationObserver(mutationCallback)
+observer.observe(targetNode, obConfig)
