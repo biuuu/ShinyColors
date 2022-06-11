4c65c5c1152789a104e7f67ed38ca4c49720a5ae
mio <10892119+biuuu@users.noreply.github.com>
2022-06-11 19:52:10 +0800

    extension: use manifestV2 & invalidate Object.freeze

--- extension/content-script.js
+++ extension/content-script.js
@@ -1,23 +1,10 @@
-const mutationCallback = (mutationsList) => {
-  for (let mutation of mutationsList) {
-    const addedNodes = mutation.addedNodes
-    addedNodes.forEach(node => {
-      if ('SCRIPT' === node.tagName) {
-        node.defer = false
-        if (node.src.includes('shinycolors.enza.fun/app')) {
-          node.async = true
-          observer.disconnect()
-        }
-      }
-    })
+const tag = document.createElement('script')
+tag.textContent = `const originFreeze = Object.freeze
+
+Object.freeze = new Proxy(originFreeze, {
+  apply (target, self, [data]) {
+    return data
   }
-}
+})`
 
-const obConfig = {
-  subtree: true,
-  childList: true
-}
-
-const targetNode = document
-const observer = new MutationObserver(mutationCallback)
-observer.observe(targetNode, obConfig)
+;(document.head || document.documentElement).appendChild(tag)
