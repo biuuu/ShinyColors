00e22900c85ec17c6d9f6aa08b767417b1e0eef5
inyeongjang <726jang@gmail.com>
2025-07-17 16:39:11 +0900

    [Autofic] 3 malicious code detected!!

--- script/server.js
+++ script/server.js
@@ -1,8 +1,15 @@
 const handler = require('serve-handler')
-const http = require('http')
+const https = require('https')
+const fs = require('fs')
 const open = require('open')
 
-const server = http.createServer((request, response) => {
+// SSL/TLS 인증서 및 키 파일 경로 설정
+const options = {
+  key: fs.readFileSync('path/to/your/private-key.pem'),
+  cert: fs.readFileSync('path/to/your/certificate.pem')
+}
+
+const server = https.createServer(options, (request, response) => {
   // You pass two more arguments for config and middleware
   // More details here: https://github.com/zeit/serve-handler#options
   response.setHeader('Access-Control-Allow-Origin', '*')
@@ -13,6 +20,6 @@ const server = http.createServer((request, response) => {
 })
 
 server.listen(15944, async () => {
-  console.log('Local server at http://127.0.0.1:15944')
-  await open('http://localhost:15944/ShinyColors.user.js')
+  console.log('Local server at https://127.0.0.1:15944')
+  await open('https://localhost:15944/ShinyColors.user.js')
 })
